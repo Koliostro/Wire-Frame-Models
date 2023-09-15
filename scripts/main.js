9b2fda6e2cd2cc6d5f884a7c236ef8861270d463
Роман <senior.starkoff2018@yandex.ru>
2023-09-15 19:29:37 +0300

    Spiled code for some files

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,12 @@
 import * as drawing from "/scripts/drawing.js"
 
-drawing.line(10, 10, 100, 100, 0xc9a1, 2)
+drawing.draw_tetrahedron(10)
+
+// drawing.line(100, 100, 300, 100, 0xc629, 2);
+// drawing.line(100, 100, 174, 100, 0xc629, 2);
+// drawing.line(174, 100, 300, 100, 0xc629, 2);
+// drawing.line(100, 100, 185.5, 228, 0xc629, 2);
+// drawing.line(174, 100, 185.5, 228, 0xc629, 2);
+// drawing.line(300, 100, 185.5, 228, 0xc629, 2);
 
 console.log("Done something!")
