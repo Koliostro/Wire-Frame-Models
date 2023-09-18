5d96cc6cc28e853b17183e6caf852ac78d3a828a
Роман <senior.starkoff2018@yandex.ru>
2023-09-18 20:28:52 +0300

    Small changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,21 @@
 import * as drawing from "/scripts/drawing.js"
 
-drawing.draw_tetrahedron(10)
+let vertex_of_tetrahedron = [
+    [0, 0, 0],
+    [2, 0, 3.5],
+    [4, 0, 0],
+    [2, 3, 1.7]
+]
 
-// drawing.line(100, 100, 300, 100, 0xc629, 2);
-// drawing.line(100, 100, 174, 100, 0xc629, 2);
-// drawing.line(174, 100, 300, 100, 0xc629, 2);
-// drawing.line(100, 100, 185.5, 228, 0xc629, 2);
-// drawing.line(174, 100, 185.5, 228, 0xc629, 2);
-// drawing.line(300, 100, 185.5, 228, 0xc629, 2);
+const edges_of_tetrahedron = [
+    [0, 1],
+    [0, 2],
+    [0, 3],
+    [1, 2],
+    [1, 3],
+    [2, 3]
+]
+
+drawing.drawing(vertex_of_tetrahedron, 10, edges_of_tetrahedron)
 
 console.log("Done something!")
